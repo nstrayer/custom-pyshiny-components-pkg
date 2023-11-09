a3ac6a09a38217bc80facecd9e7bea4f43ad6611
Nick Strayer <nick.strayer@rstudio.com>
2023-11-09 11:06:30 -0500

    Add custom input example and also fix malformed script names for output example

--- customShinyComponents/__init__.py
+++ customShinyComponents/__init__.py
@@ -1,3 +1,8 @@
-from .shiny_custom_output import render_custom_output, output_custom_output
+from .custom_output import render_custom_output, output_custom_output
+from .custom_input import custom_input
 
-__all__ = ["render_custom_output", "output_custom_output"]
+__all__ = [
+    "render_custom_output",
+    "output_custom_output",
+    "custom_input",
+]
