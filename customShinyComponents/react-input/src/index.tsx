1ca592e99a59bb20cc4ab70423a6bb53dfc0ef9b
Nick Strayer <nick.strayer@rstudio.com>
2023-11-13 11:29:00 -0500

    Make react example color picker from ShinyComponent

--- customShinyComponents/react-input/src/index.tsx
+++ customShinyComponents/react-input/src/index.tsx
@@ -1,49 +1,45 @@
 import { createRoot } from "react-dom/client";
 import { addStylesForTag } from "./addStylesForTag";
-
+import { SketchPicker } from "react-color";
+import type { ColorResult } from "react-color";
+import React from "react";
 const customInputTag = "custom-react-input";
 
-const styles = `
-${customInputTag} {
-  display: block;
-  border: solid 1px gray;
-  padding: 16px;
-  width: fit-content;
-}
-
-${customInputTag} .container {
-  display: flex;
-  gap: 1rem;
-  align-items: center;
-}
-`;
-
 /**
- * An example element that renders a react component.
+ * A webcomponent that wraps a react component. This is the component that
+ * Shiny will interact with. By using a webcomponent here we can encapsulate
+ * all the dependencies and logic for the react component and only expose a
+ * simple HTML tag interface to Shiny.
  */
 class ShinyReactComponent extends HTMLElement {
   /**
    * The current value of the input.
    */
-  value = 0;
+  value: string = "#fff";
+
+  onChangeCallback: () => void = () => null;
 
   constructor() {
     super();
-
     // Add styles to the dom for this component
-    addStylesForTag(customInputTag, styles);
+    addStylesForTag(
+      customInputTag,
+      `${customInputTag} {
+          display: block;
+          border: solid 1px gray;
+          padding: 16px;
+          width: fit-content;
+        }`
+    );
   }
 
   /**
-   * Function to run when the increment button is clicked.
+   * Function to run when the a new color is shown. First updates the value and
+   * then tells Shiny that there is an updated value
    */
-  onIncrement() {
-    this.value++;
-
-    // Emit a custom event saying this tag has changed.
-    // This event is subscribed to by the shiny input binding
-    // to alert shiny that the value has changed.
-    this.dispatchEvent(new CustomEvent("input-change"));
+  onNewColor(color: string) {
+    this.value = color;
+    this.onChangeCallback();
   }
 
   connectedCallback() {
@@ -51,45 +47,59 @@ class ShinyReactComponent extends HTMLElement {
     // Note the use of arrow functions. This makes sure the `this` stays the
     // webcomponent and doesn't get bound away by react.
     createRoot(this).render(
-      <div className="container">
-        <span>I'm being rendered by react</span>
-        <button onClick={() => this.onIncrement()}>Click me!</button>
-      </div>
+      <ColorPickerReact
+        initialColor={this.value}
+        currentColorCallback={(color) => this.onNewColor(color)}
+      />
     );
   }
+}
+
+// Color Picker React component
+function ColorPickerReact({
+  initialColor,
+  currentColorCallback,
+}: {
+  initialColor: string;
+  currentColorCallback: (x: string) => void;
+}) {
+  const [currentColor, setCurrentColor] = React.useState<string>(initialColor);
+
+  function handleChange(color: ColorResult) {
+    setCurrentColor(color.hex);
+    currentColorCallback(color.hex);
+  }
+
+  return <SketchPicker color={currentColor} onChange={handleChange} />;
 }
 
 customElements.define(customInputTag, ShinyReactComponent);
 
 // Setup the input binding for the custom input
 class ReactWithLitBinding extends Shiny.InputBinding {
-  constructor() {
-    super();
-  }
-
   override find(scope: HTMLElement): JQuery<HTMLElement> {
     return $(scope).find(customInputTag);
   }
 
   override getValue(el: ShinyReactComponent) {
+    // Our component stores the value in the `value` property. In this case it's
+    // a string with the current color.
     return el.value;
   }
-
-  onChangeCallback: () => void = () => null;
 
   override subscribe(
     el: ShinyReactComponent,
     callback: (x: boolean) => void
   ): void {
-    // Store the callback so we can unsubscribe later
-    this.onChangeCallback = () => callback(true);
-
-    // Subscribe to the custom event that is emitted when the value changes.
-    el.addEventListener("change", this.onChangeCallback);
+    // Hook up callback used to tell Shiny that the value has changed
+    // to the onChangeCallback of the webcomponent we created
+    el.onChangeCallback = () => callback(true);
   }
 
   override unsubscribe(el: ShinyReactComponent): void {
-    el.removeEventListener("change", this.onChangeCallback);
+    // Remove the value-has-updated callback so that it doesn't get called
+    // after Shiny is no longer bound to the element
+    el.onChangeCallback = () => null;
   }
 }
 
